57ffd9b7623d87f5fa8847c88106ff5b434b71f1
Piotr Sowiński <56843159+Ostrzyciel@users.noreply.github.com>
2023-01-21 15:50:48 +0100

    Add JellyOptionsFromTypesafe to stream (#12)
    This would better fit into core... but core does not import the typesafe
    config library, so meh. Stream does (because it relies on Akka), so I
    put it there.

---
+++ stream/src/main/scala/eu/ostrzyciel/jelly/stream/JellyOptionsFromTypesafe.scala
@@ -0,0 +1,49 @@
+package eu.ostrzyciel.jelly.stream
+
+import com.typesafe.config.{Config, ConfigFactory}
+import eu.ostrzyciel.jelly.core.proto.v1.{RdfStreamOptions, RdfStreamType}
+
+/**
+ * Convenience methods for building Jelly's options ([[RdfStreamOptions]]) from [[com.typesafe.Config]].
+ *
+ * See also [[eu.ostrzyciel.jelly.core.JellyOptions]]
+ */
+object JellyOptionsFromTypesafe:
+  private val defaultConfig = ConfigFactory.parseString("""
+    |stream-type = UNSPECIFIED
+    |generalized-statements = false
+    |use-repeat = true
+    |name-table-size = 128
+    |prefix-table-size = 16
+    |dt-table-size = 16
+    |""".stripMargin)
+
+  /**
+   * Builds RdfStreamOptions from a typesafe config instance.
+   *
+   * @param config typesafe config with keys:
+   *               - "stream-type", either UNSPECIFIED, TRIPLES, QUADS, or GRAPHS. Default: UNSPECIFIED.
+   *               - "generalized-statements", boolean. Default: false.
+   *               - "use-repeat", boolean. Default: true.
+   *               - "name-table-size", integer. Default: 128.
+   *               - "prefix-table-size", integer. Default: 16.
+   *               - "dt-table-size", integer. Default: 16.
+   * @return
+   */
+  def fromTypesafeConfig(config: Config): RdfStreamOptions =
+    val merged = config.withFallback(defaultConfig)
+    RdfStreamOptions(
+      streamType = (
+        merged.getString("stream-type") match
+          case "UNSPECIFIED" => RdfStreamType.RDF_STREAM_TYPE_UNSPECIFIED
+          case "TRIPLES" => RdfStreamType.RDF_STREAM_TYPE_TRIPLES
+          case "QUADS" => RdfStreamType.RDF_STREAM_TYPE_QUADS
+          case "GRAPHS" => RdfStreamType.RDF_STREAM_TYPE_GRAPHS
+          case _ => throw IllegalArgumentException()
+        ),
+      generalizedStatements = merged.getBoolean("generalized-statements"),
+      useRepeat = merged.getBoolean("use-repeat"),
+      maxNameTableSize = merged.getInt("name-table-size"),
+      maxPrefixTableSize = merged.getInt("prefix-table-size"),
+      maxDatatypeTableSize = merged.getInt("dt-table-size"),
+    )
