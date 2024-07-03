71055748265bbd1057d56f22bb47e0ca814bcedc
Ostrzyciel <ostrzycielnozyczek@gmail.com>
2024-07-03 14:21:33 +0200

    cleanup

--- core/src/main/scala/eu/ostrzyciel/jelly/core/Constants.scala
+++
@@ -1,8 +0,0 @@
-package eu.ostrzyciel.jelly.core
-
-object Constants:
-  val jellyName = "Jelly"
-  val jellyFileExtension = "jelly"
-  val jellyContentType = "application/x-jelly-rdf"
-  val protoVersion = 1
-  val protoSemanticVersion = "1.0.0"
--- rdf4j/src/main/scala/eu/ostrzyciel/jelly/convert/rdf4j/package.scala
+++
@@ -1,5 +0,0 @@
-package eu.ostrzyciel.jelly.convert
-
-package object rdf4j:
-  given rdf4jIterableAdapter: Rdf4jIterableAdapter.type = Rdf4jIterableAdapter
-  given rdf4jConverterFactory: Rdf4jConverterFactory.type = Rdf4jConverterFactory
