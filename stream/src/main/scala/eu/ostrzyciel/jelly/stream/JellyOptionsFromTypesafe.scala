77b354df632e67366ad31ff7ec1b8d6c4d4200cc
Piotr Sowiński <ostrzycielnozyczek@gmail.com>
2024-07-09 09:07:18 +0200

    Update and improve JellyOptionsFromTypesafe (#104)
    Improved:
    - More consistent config option naming
    - Support for logical stream types
    - Removed reliance on hardcoded cases, use maps to determine the
      physical and logical stream types.
    - Better exception messages.

--- stream/src/main/scala/eu/ostrzyciel/jelly/stream/JellyOptionsFromTypesafe.scala
+++ stream/src/main/scala/eu/ostrzyciel/jelly/stream/JellyOptionsFromTypesafe.scala
@@ -1,7 +1,7 @@
 package eu.ostrzyciel.jelly.stream
 
 import com.typesafe.config.{Config, ConfigFactory}
-import eu.ostrzyciel.jelly.core.proto.v1.{RdfStreamOptions, PhysicalStreamType}
+import eu.ostrzyciel.jelly.core.proto.v1.{LogicalStreamType, PhysicalStreamType, RdfStreamOptions}
 
 /**
  * Convenience methods for building Jelly's options ([[RdfStreamOptions]]) from [[com.typesafe.Config]].
@@ -10,40 +10,49 @@ import eu.ostrzyciel.jelly.core.proto.v1.{RdfStreamOptions, PhysicalStreamType}
  */
 object JellyOptionsFromTypesafe:
   private val defaultConfig = ConfigFactory.parseString("""
-    |stream-type = UNSPECIFIED
+    |physical-type = UNSPECIFIED
+    |logical-type = UNSPECIFIED
     |generalized-statements = false
     |rdf-star = false
     |name-table-size = 128
     |prefix-table-size = 16
-    |dt-table-size = 16
+    |datatype-table-size = 16
     |""".stripMargin)
+  
+  private val physicalStreamTypeMap = PhysicalStreamType.values
+    .map(v => v.name.replace("PHYSICAL_STREAM_TYPE_", "") -> v)
+    .toMap
+  
+  private val logicalStreamTypeMap = LogicalStreamType.values
+    .map(v => v.name.replace("LOGICAL_STREAM_TYPE_", "") -> v)
+    .toMap
 
   /**
    * Builds RdfStreamOptions from a typesafe config instance.
    *
    * @param config typesafe config with keys:
-   *               - "stream-type", either UNSPECIFIED, TRIPLES, QUADS, or GRAPHS. Default: UNSPECIFIED.
+   *               - "physical-type", either UNSPECIFIED, TRIPLES, QUADS, or GRAPHS. Default: UNSPECIFIED.
+   *               - "logical-type", one of the defined logical types like "FLAT_TRIPLES" in rdf.proto,
+   *                  LogicalStreamType enum. Default: UNSPECIFIED.
    *               - "generalized-statements", boolean. Default: false.
    *               - "rdf-star", boolean. Default: false.
    *               - "name-table-size", integer. Default: 128.
    *               - "prefix-table-size", integer. Default: 16.
-   *               - "dt-table-size", integer. Default: 16.
+   *               - "datatype-table-size", integer. Default: 16.
    * @return
    */
   def fromTypesafeConfig(config: Config): RdfStreamOptions =
     val merged = config.withFallback(defaultConfig)
     RdfStreamOptions(
-      physicalType = (
-        merged.getString("stream-type") match
-          case "UNSPECIFIED" => PhysicalStreamType.UNSPECIFIED
-          case "TRIPLES" => PhysicalStreamType.TRIPLES
-          case "QUADS" => PhysicalStreamType.QUADS
-          case "GRAPHS" => PhysicalStreamType.GRAPHS
-          case _ => throw IllegalArgumentException()
-        ),
+      physicalType = physicalStreamTypeMap.get(merged.getString("physical-type")) match
+        case Some(v) => v
+        case None => throw new IllegalArgumentException(s"Unknown physical type: ${merged.getString("physical-type")}"),
+      logicalType = logicalStreamTypeMap.get(merged.getString("logical-type")) match
+        case Some(v) => v
+        case None => throw new IllegalArgumentException(s"Unknown logical type: ${merged.getString("logical-type")}"),
       generalizedStatements = merged.getBoolean("generalized-statements"),
       rdfStar = merged.getBoolean("rdf-star"),
       maxNameTableSize = merged.getInt("name-table-size"),
       maxPrefixTableSize = merged.getInt("prefix-table-size"),
-      maxDatatypeTableSize = merged.getInt("dt-table-size"),
+      maxDatatypeTableSize = merged.getInt("datatype-table-size"),
     )
