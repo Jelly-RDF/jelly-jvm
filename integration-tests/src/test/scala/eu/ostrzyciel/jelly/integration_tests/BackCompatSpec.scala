3eba2c6d787647e562bba12b12372f91ea5eac9d
Ostrzyciel <ostrzycielnozyczek@gmail.com>
2024-07-18 14:01:35 +0200

    Add backward compatibility parser tests
    Continuation of #118. Added a few basic tests to make sure files saved by a previous release of Jelly are still readable by a newer one. These tests should be expanded later.

---
+++ integration-tests/src/test/scala/eu/ostrzyciel/jelly/integration_tests/BackCompatSpec.scala
@@ -0,0 +1,42 @@
+package eu.ostrzyciel.jelly.integration_tests
+
+import eu.ostrzyciel.jelly.convert.jena.riot.JellyLanguage
+import org.apache.jena.riot.{Lang, RDFDataMgr}
+import org.apache.jena.sparql.core.DatasetGraphFactory
+import org.apache.jena.sys.JenaSystem
+import org.scalatest.BeforeAndAfterAll
+import org.scalatest.concurrent.ScalaFutures
+import org.scalatest.matchers.should.Matchers
+import org.scalatest.wordspec.AnyWordSpec
+
+import scala.jdk.CollectionConverters.*
+
+class BackCompatSpec extends AnyWordSpec, Matchers, ScalaFutures, BeforeAndAfterAll:
+  override def beforeAll(): Unit =
+    JenaSystem.init()
+
+  private val testCases = Seq(
+    ("riverbench_main", "RiverBench main metadata", Seq("v1_0_0")),
+    ("riverbench_nanopubs", "RiverBench nanopubs dataset metadata", Seq("v1_0_0")),
+    ("weather_quads", "Jelly 1.0.0, weather data example (QUADS stream)", Seq("v1_0_0")),
+  )
+
+  for (fileName, description, versions) <- testCases do
+  for version <- versions do
+    s"Backward compatibility with $description" should {
+      s"be maintained for Jelly version $version when parsing" in {
+        val jellyDg = DatasetGraphFactory.create()
+        RDFDataMgr.read(
+          jellyDg,
+          getClass.getResourceAsStream(s"/backcompat/${fileName}_$version.jelly"),
+          JellyLanguage.JELLY
+        )
+        val jenaDg = DatasetGraphFactory.create()
+        RDFDataMgr.read(
+          jenaDg,
+          getClass.getResourceAsStream(s"/backcompat/$fileName.trig"),
+          Lang.TRIG
+        )
+        CrossStreamingSpec.compareDatasets(jellyDg, jenaDg)
+      }
+    }
