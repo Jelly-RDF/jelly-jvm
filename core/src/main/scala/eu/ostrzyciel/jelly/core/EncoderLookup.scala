d1f2ee0962273197d0ff4b1ad25fc7a9e4ecc2d1
Ostrzyciel <ostrzycielnozyczek@gmail.com>
2024-07-18 08:52:52 +0200

    Experiment: cache last accessed value in EncoderLookup
    MAYBE this will help the serialization performance, we'll see.

--- core/src/main/scala/eu/ostrzyciel/jelly/core/EncoderLookup.scala
+++ core/src/main/scala/eu/ostrzyciel/jelly/core/EncoderLookup.scala
@@ -9,6 +9,9 @@ private[core] final class EncoderLookup(maxEntries: Int)
 
   // 1-based index of the last assigned new value in the lookup table
   private var lastSetId: Int = 0
+  private var lastAccessedKeyHash: Int = -1
+  private var lastAccessedKey: String = ""
+  private var lastAccessedValue: EncoderValue = null
 
   override def removeEldestEntry(eldest: util.Map.Entry[String, EncoderValue]): Boolean =
     size > maxEntries
@@ -19,23 +22,33 @@ private[core] final class EncoderLookup(maxEntries: Int)
    * @return (1-based id of the value, is a new entry)
    */
   def addEntry(v: String): EncoderValue =
+    val vHash = v.hashCode
+    if lastAccessedKeyHash == vHash && lastAccessedKey == v then
+      // case 1: the value is the same as the last accessed one
+      return lastAccessedValue
+
+    lastAccessedKey = v
+    lastAccessedKeyHash = vHash
+
     val value = this.get(v)
     if value != null then
-      // case 1: the value is already in the map
+      // case 2: the value is already in the map
+      lastAccessedValue = value
       return value
 
     val s = this.size
     if s < maxEntries then
-      // case 2: we still have free IDs, add it to the map
+      // case 3: we still have free IDs, add it to the map
       lastSetId = s + 1
-      this.put(v, EncoderValue(lastSetId, lastSetId, false))
+      lastAccessedValue = EncoderValue(lastSetId, lastSetId, false)
+      this.put(v, lastAccessedValue)
       // the setId is always 0, because we haven't filled in the table yet
       return EncoderValue(lastSetId, 0, true)
 
-    // case 3: no free IDs, reuse an old one
-    val next = this.values.iterator.next
-    this.put(v, next)
-    val getId = next.getId
+    // case 4: no free IDs, reuse an old one
+    lastAccessedValue = this.values.iterator.next
+    this.put(v, lastAccessedValue)
+    val getId = lastAccessedValue.getId
     val setId = if lastSetId + 1 == getId then 0 else getId
     lastSetId = getId
     EncoderValue(getId, setId, true)
