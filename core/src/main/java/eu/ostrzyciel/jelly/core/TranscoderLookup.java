cced3e548f0aacd3f4f2611dfe312ab1445e6ed1
Ostrzyciel <ostrzycielnozyczek@gmail.com>
2024-11-26 14:29:42 +0100

    Finish tests for TranscoderLookup
    100% coverage

--- core/src/main/java/eu/ostrzyciel/jelly/core/TranscoderLookup.java
+++ core/src/main/java/eu/ostrzyciel/jelly/core/TranscoderLookup.java
@@ -10,7 +10,6 @@ class TranscoderLookup {
     //    stream. While splitting, we need to check for zeroes at the start of the stream and remap them.
     //  - IRI names: remap all 0s forcefully
     private final boolean isNameLookup;
-    private boolean isFirstElement = true;
     private int lastSetId = 0;
     private int lastInputGetId = 0;
     private int lastOutputGetId = 0;
@@ -35,15 +34,10 @@ class TranscoderLookup {
     int remap(int id) {
         if (isNameLookup) {
             if (id == 0) {
-                if (isFirstElement) {
-                    lastInputGetId = id = 1;
-                } else {
-                    id = ++lastInputGetId;
-                }
+                id = ++lastInputGetId;
             } else {
                 lastInputGetId = id;
             }
-            isFirstElement = false;
             int outputId = table[id];
             lookup.onAccess(outputId);
             if (outputId == lastOutputGetId + 1) {
@@ -55,17 +49,10 @@ class TranscoderLookup {
         }
         if (id == 0) {
             // No need to do onAccess here, because this is the same as the last element
-            if (isFirstElement) {
-                // We are starting a new output stream, so we need to remap the first zero.
-                isFirstElement = false;
-                return lastInputGetId;
-            }
             return 0;
         }
-        lastInputGetId = id;
         id = table[id];
         lookup.onAccess(id);
-        isFirstElement = false;
         return id;
     }
 
@@ -73,9 +60,13 @@ class TranscoderLookup {
         if (size > outputSize) {
             throw new IllegalArgumentException("Input lookup size cannot be greater than the output lookup size");
         }
+        if (table != null) {
+            // Only set this for streams 2 and above (counting from 1)
+            lastSetId = 0;
+            lastInputGetId = 0;
+        }
         if (table == null || table.length < size + 1) {
             table = new int[size + 1];
         }
-        isFirstElement = true;
     }
 }
