378b0f5db3ce01c0a1ab3d45dcab6cc77b78d7fb
Ostrzyciel <ostrzycielnozyczek@gmail.com>
2025-02-16 22:07:53 +0100

    WIP: RDF Patch implementation
    Turns out, lots of preparatory work in core will be required.

---
+++ core-patch/src/main/scala/eu/ostrzyciel/jelly/core/patch/PatchEncoder.scala
@@ -0,0 +1,35 @@
+package eu.ostrzyciel.jelly.core.patch
+
+import eu.ostrzyciel.jelly.core.proto.v1.*
+
+import scala.collection.mutable
+
+object PatchEncoder:
+  final case class Params[TNode, -TTriple, -TQuad, -TQuoted](
+    options: RdfPatchOptions,
+    rowBuffer: mutable.Buffer[RdfPatchRow],
+    converter: PatchEncoderConverter[TNode, TTriple, TQuad, TQuoted],
+  )
+
+trait PatchEncoder[TNode, -TTriple, -TQuad, -TQuoted]:
+  val options: RdfPatchOptions
+
+  def addTripleStatement(triple: TTriple): Unit
+
+  def deleteTripleStatement(triple: TTriple): Unit
+
+  def addQuadStatement(quad: TQuad): Unit
+
+  def deleteQuadStatement(quad: TQuad): Unit
+
+  def transactionStart(): Unit
+
+  def transactionCommit(): Unit
+
+  def transactionAbort(): Unit
+
+  def addNamespace(name: String, iriValue: String): Unit
+
+  def deleteNamespace(name: String, iriValue: String): Unit
+
+  def header(key: String, value: TNode): Unit
