98be1b0a1fdf9dcda6aae84c334be4e7b98f2b94
Ostrzyciel <ostrzycielnozyczek@gmail.com>
2025-03-01 17:37:52 +0100

    tests WIP

--- core-patch/src/test/scala/eu/ostrzyciel/jelly/core/patch/PatchEncoderSpec.scala
+++ core-patch/src/test/scala/eu/ostrzyciel/jelly/core/patch/PatchEncoderSpec.scala
@@ -1,5 +1,6 @@
 package eu.ostrzyciel.jelly.core.patch
 
+import eu.ostrzyciel.jelly.core.patch.helpers.Assertions.*
 import eu.ostrzyciel.jelly.core.patch.helpers.MockPatchConverterFactory
 import eu.ostrzyciel.jelly.core.proto.v1.patch.*
 import org.scalatest.matchers.should.Matchers
@@ -8,6 +9,7 @@ import org.scalatest.wordspec.AnyWordSpec
 import scala.collection.mutable.ListBuffer
 
 class PatchEncoderSpec extends AnyWordSpec, Matchers:
+  import eu.ostrzyciel.jelly.core.patch.helpers.PatchTestCases.*
   import PatchEncoder.Params as Pep
   
   "a PatchEncoder" should {
@@ -17,5 +19,17 @@ class PatchEncoderSpec extends AnyWordSpec, Matchers:
         JellyPatchOptions.smallGeneralized.withPhysicalType(PatchPhysicalType.TRIPLES),
         buffer
       ))
+      Triples1.mrl.foreach(_.apply(encoder))
+      assertEncoded(buffer.toSeq, Triples1.encoded(encoder.options.withVersion(PatchConstants.protoVersion)))
+    }
+
+    "encode a triple patch with namespace declarations" in {
+      val buffer = ListBuffer[RdfPatchRow]()
+      val encoder = MockPatchConverterFactory.patchEncoder(Pep(
+        JellyPatchOptions.smallGeneralized.withPhysicalType(PatchPhysicalType.TRIPLES),
+        buffer,
+      ))
+      Triples2NsDecl.mrl.foreach(_.apply(encoder))
+      assertEncoded(buffer.toSeq, Triples2NsDecl.encoded(encoder.options))
     }
   }
