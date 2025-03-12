ed9bf4f2ff3ff0deeb7ff441d08ac829c570d61d
Ostrzyciel <ostrzycielnozyczek@gmail.com>
2025-03-12 20:51:35 +0100

    I/O WIP

--- titanium-rdf-api/src/main/scala/eu/ostrzyciel/jelly/convert/titanium/Blep.scala
+++
@@ -1,6 +0,0 @@
-package eu.ostrzyciel.jelly.convert.titanium
-
-import eu.ostrzyciel.jelly.convert.titanium.internal.*
-
-class Blep:
-  TitaniumConverterFactory.encoder(null, true).addTripleStatement()
