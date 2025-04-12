bad988f56a1cfa43eac218e1367ca55fc5d4cd24
Ostrzyciel <ostrzycielnozyczek@gmail.com>
2025-04-12 12:35:51 +0200

    finish Jena interop

--- jena-patch/src/main/scala/eu/ostrzyciel/jelly/convert/jena/patch/JellyPatchOps.scala
+++ jena-patch/src/main/scala/eu/ostrzyciel/jelly/convert/jena/patch/JellyPatchOps.scala
@@ -2,8 +2,9 @@ package eu.ostrzyciel.jelly.convert.jena.patch
 
 import eu.ostrzyciel.jelly.core.patch.handler.AnyPatchHandler
 import org.apache.jena.graph.Node
-import org.apache.jena.rdfpatch.RDFChanges
+import org.apache.jena.rdfpatch.{RDFChanges, RDFPatch}
 
+import java.io.{InputStream, OutputStream}
 import scala.annotation.experimental
 
 @experimental
@@ -14,4 +15,23 @@ object JellyPatchOps:
 
   def fromJenaToJelly(jellyStream: AnyPatchHandler[Node]): RDFChanges = JenaToJelly(jellyStream)
 
-  // TODO: readers, writers, ...
+  def read(in: InputStream, destination: RDFChanges): Unit =
+    read(in, destination, RdfPatchReaderJelly.Options())
+
+  def read(in: InputStream, destination: RDFChanges, opt: RdfPatchReaderJelly.Options): Unit =
+    val reader = RdfPatchReaderJelly(opt, in)
+    reader.apply(destination)
+
+  def write(out: OutputStream, patch: RDFPatch): Unit =
+    write(out, patch, RdfPatchWriterJelly.Options())
+
+  def write(out: OutputStream, patch: RDFPatch, opt: RdfPatchWriterJelly.Options): Unit =
+    val w = writer(out, opt)
+    patch.apply(w)
+    w.finish()
+
+  def writer(out: OutputStream): RDFChanges =
+    writer(out, RdfPatchWriterJelly.Options())
+
+  def writer(out: OutputStream, opt: RdfPatchWriterJelly.Options): RDFChanges =
+    RdfPatchWriterJellyAutodetectType(opt, out)
