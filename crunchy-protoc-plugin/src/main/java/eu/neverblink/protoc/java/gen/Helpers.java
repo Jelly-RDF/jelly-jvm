7303aaf17cbe3e73ec748877930d272734b7188c
Ostrzyciel <ostrzycielnozyczek@gmail.com>
2025-05-04 14:25:54 +0200

    Formatting fix

--- crunchy-protoc-plugin/src/main/java/eu/neverblink/protoc/java/gen/Helpers.java
+++ crunchy-protoc-plugin/src/main/java/eu/neverblink/protoc/java/gen/Helpers.java
@@ -4,5 +4,6 @@ import com.palantir.javapoet.ArrayTypeName;
 import com.palantir.javapoet.TypeName;
 
 public class Helpers {
+
     public static final TypeName TYPE_BYTE_ARRAY = ArrayTypeName.get(byte[].class);
 }
