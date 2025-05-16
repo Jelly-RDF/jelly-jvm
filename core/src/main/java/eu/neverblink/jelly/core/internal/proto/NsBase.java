efe1b711e9a7d9ef44cbd0905a1bbafc0f146f60
Nik Kozlov <nikkoz893@gmail.com>
2025-05-16 19:46:12 +0200

    Annotate all internal classes with InternalApi

--- core/src/main/java/eu/neverblink/jelly/core/internal/proto/NsBase.java
+++ core/src/main/java/eu/neverblink/jelly/core/internal/proto/NsBase.java
@@ -1,7 +1,9 @@
 package eu.neverblink.jelly.core.internal.proto;
 
+import eu.neverblink.jelly.core.InternalApi;
 import eu.neverblink.jelly.core.proto.v1.RdfIri;
 
+@InternalApi
 public interface NsBase {
     interface Setters extends NsBase {
         NsBase setValue(RdfIri iri);
--- core/src/main/java/eu/neverblink/jelly/core/internal/proto/OptionsBase.java
+++ core/src/main/java/eu/neverblink/jelly/core/internal/proto/OptionsBase.java
@@ -1,5 +1,8 @@
 package eu.neverblink.jelly.core.internal.proto;
 
+import eu.neverblink.jelly.core.InternalApi;
+
+@InternalApi
 public interface OptionsBase {
     boolean getGeneralizedStatements();
     boolean getRdfStar();
