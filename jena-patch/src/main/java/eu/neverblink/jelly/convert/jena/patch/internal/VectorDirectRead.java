62a532d934d0f14e5383476307ab22cb8aacf82f
Ostrzyciel <ostrzycielnozyczek@gmail.com>
2025-07-23 13:07:47 +0200

    prettier

--- jena-patch/src/main/java/eu/neverblink/jelly/convert/jena/patch/internal/VectorDirectRead.java
+++ jena-patch/src/main/java/eu/neverblink/jelly/convert/jena/patch/internal/VectorDirectRead.java
@@ -1,18 +1,18 @@
 package eu.neverblink.jelly.convert.jena.patch.internal;
 
 import eu.neverblink.jelly.core.InternalApi;
-
 import java.util.Vector;
 
 @InternalApi
 public class VectorDirectRead<E> extends Vector<E> {
+
     /**
      * Returns the internal array of the vector without copying it. Do not modify the array!
      * The array may be larger than the current size of the vector, use .size() to determine the actual size.
      * <p>
      * This method makes sense only if you are absolutely sure that the vector is not modified
      * after this call, as it returns the internal array directly.
-     * 
+     *
      * @return the internal array of the vector
      */
     public Object[] getArrayUnsafe() {
