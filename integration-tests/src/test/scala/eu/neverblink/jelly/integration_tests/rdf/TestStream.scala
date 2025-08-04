236c78397f362c578a0578ad91763e70b2dd9b50
niegrzybkowski <kacper.grzymkowski@neverblink.eu>
2025-08-04 13:45:08 +0200

    Add comments about supportsRdf12

--- integration-tests/src/test/scala/eu/neverblink/jelly/integration_tests/rdf/TestStream.scala
+++ integration-tests/src/test/scala/eu/neverblink/jelly/integration_tests/rdf/TestStream.scala
@@ -25,5 +25,10 @@ trait TestStream:
   def graphSink(os: OutputStream)(using ExecutionContext): Sink[RdfStreamFrame, Future[Done]]
 
   def supportsRdfStar: Boolean = true
-  
+
+  /** This is needed because Jena suddenly dropped RDF-star support in 5.4.
+   * While Jelly doesn't support RDF1.2, some RDF-star test cases can be translated 1:1 to RDF1.2.
+   * This allows some testing of quoted triples even with Jena 5.4+
+   * See: https://github.com/Jelly-RDF/jelly-jvm/issues/368
+   */
   def supportsRdf12: Boolean = false
--- integration-tests/src/test/scala/eu/neverblink/jelly/integration_tests/rdf/io/NativeSerDes.scala
+++ integration-tests/src/test/scala/eu/neverblink/jelly/integration_tests/rdf/io/NativeSerDes.scala
@@ -15,4 +15,10 @@ trait NativeSerDes[TModel : Measure, TDataset : Measure]:
   def writeQuadsJelly(os: OutputStream, dataset: TDataset, opt: Option[RdfStreamOptions], frameSize: Int): Unit
   def supportsRdfStar: Boolean = true
   def supportsGeneralizedStatements: Boolean = true
+
+  /** This is needed because Jena suddenly dropped RDF-star support in 5.4.
+   * While Jelly doesn't support RDF1.2, some RDF-star test cases can be translated 1:1 to RDF1.2.
+   * This allows some testing of quoted triples even with Jena 5.4+
+   * See: https://github.com/Jelly-RDF/jelly-jvm/issues/368
+   */
   def supportsRdf12: Boolean = false
