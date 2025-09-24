7d4e801fe3f4f0e57be84bf8a65ee0741a00fd0c
niegrzybkowski <kacper.grzymkowski@neverblink.eu>
2025-09-24 14:18:04 +0200

    Stuff

--- integration-tests/src/test/scala/eu/neverblink/jelly/integration_tests/util/ConformanceReporter.scala
+++ integration-tests/src/test/scala/eu/neverblink/jelly/integration_tests/util/ConformanceReporter.scala
@@ -1,38 +1,36 @@
 package eu.neverblink.jelly.integration_tests.util
 
 import eu.neverblink.jelly.integration_tests.rdf.io.*
-import org.apache.jena.datatypes.xsd.XSDDatatype
-import org.apache.jena.rdf.model.{AnonId, Model, ModelFactory, Property}
-import org.apache.pekko.actor.ActorSystem
+import org.apache.jena.Jena
 import org.scalatest.Reporter
-import org.scalatest.events.{Event, SuiteCompleted, TestCanceled, TestFailed, TestSucceeded}
+import org.scalatest.events.*
 
 import java.nio.file.{Files, Paths}
 import java.time.{Instant, ZoneOffset}
-import scala.util.matching.Regex
 import scala.collection.mutable
+import scala.util.matching.Regex
 
 class ConformanceReporter extends Reporter {
-  lazy val jellyV = Option(System.getenv("JELLY_VERSION")).getOrElse("dev")
-  lazy val jellyDate = Option(System.getenv("JELLY_DATE")).getOrElse(
+  lazy val jellyV: String = Option(System.getenv("JELLY_VERSION")).getOrElse("dev")
+  lazy val jellyDate: String = Option(System.getenv("JELLY_DATE")).getOrElse(
     Instant.now().atZone(ZoneOffset.UTC).toLocalDate.toString,
   )
 
   lazy val results: Map[String, mutable.StringBuilder] = Map(
-    JenaSerDes.name -> initStringBuilder(JenaSerDes.name),
+    JenaStreamSerDes.name -> initStringBuilder(JenaStreamSerDes.name),
     Rdf4jSerDes.name -> initStringBuilder(Rdf4jSerDes.name),
     "Reactive (RDF4J)" -> initStringBuilder("Reactive (RDF4J)"),
     "Reactive writes (Apache Jena)" -> initStringBuilder("Reactive writes (Apache Jena)"),
     TitaniumSerDes.name -> initStringBuilder(TitaniumSerDes.name),
   )
 
-  def renameIntegrations(name: String) = name match {
+  def renameIntegrations(name: String): String = name match {
+    case "Jena (StreamRDF)" => s"Jena ${Jena.VERSION}"
     case "Reactive (RDF4J)" => "Pekko Streams (RDF4J)"
-    case "Reactive writes (Apache Jena)" => "Pekko Streams (Jena)"
+    case "Reactive writes (Apache Jena)" => s"Pekko Streams (Jena ${Jena.VERSION})"
     case s => s
   }
 
-  val model: Model = ModelFactory.createDefaultModel()
   val prefixes: String =
     """PREFIX earl: <http://www.w3.org/ns/earl#>
       |PREFIX doap: <http://usefulinc.com/ns/doap#>
@@ -41,14 +39,23 @@ class ConformanceReporter extends Reporter {
       |PREFIX xsd:  <http://www.w3.org/2001/XMLSchema#>
       |""".stripMargin
 
+  def metadata: String =
+    s"""
+       |<> foaf:primaryTopic <#impl> ;
+       |   dc:issued "${Instant.now().atZone(ZoneOffset.UTC).toLocalDateTime}"^^xsd:dateTime ;
+       |   foaf:maker <#assertor> .
+       |""".stripMargin
+
   val assertor: String =
-    """<#assertor> a earl:Software, earl:Assertor ;
+    """
+      |<#assertor> a earl:Software, earl:Assertor ;
       |  doap:name "Jelly-JVM integration test suite" ;
       |  doap:homepage <https://github.com/Jelly-RDF/jelly-jvm/tree/main/integration-tests> .
       |""".stripMargin
 
   def formatTestSubject(name: String): String =
-    s"""<#impl> a doap:Project, earl:TestSubject, earl:Software ;
+    s"""
+       |<#impl> a doap:Project, earl:TestSubject, earl:Software ;
        |  doap:name "Jelly-JVM ($name)" ;
        |  doap:homepage <https://w3id.org/jelly/jelly-jvm/dev/> ;
        |  doap:description "Jelly-JVM integration with $name"@en ;
@@ -61,9 +68,26 @@ class ConformanceReporter extends Reporter {
        |  ] .
        |""".stripMargin
 
+  def formatResult(testName: String, ts: Long, outcome: String): String = {
+    val date = Instant.ofEpochMilli(ts).atZone(
+      ZoneOffset.UTC,
+    ).toLocalDateTime.toString
+    s"""
+       |<#${testName.replace('/', '_')}> a earl:Assertion ;
+       |  earl:assertedBy <#assertor> ;
+       |  earl:subject    <#impl> ;
+       |  earl:test       <https://github.com/Jelly-RDF/jelly-protobuf/tree/main/test/$testName> ;
+       |  earl:mode       earl:automatic ;
+       |  earl:result [
+       |    a earl:TestResult ;
+       |    dc:date "$date"^^xsd:dateTime ;
+       |    earl:outcome earl:$outcome
+       |  ] .
+       |""".stripMargin
+  }
+
   def initStringBuilder(name: String): mutable.StringBuilder = {
     val sb = mutable.StringBuilder()
-    sb.append(prefixes)
     sb.append(assertor)
     sb.append(formatTestSubject(renameIntegrations(name)))
     sb
@@ -73,22 +97,26 @@ class ConformanceReporter extends Reporter {
 
   override def apply(event: Event): Unit = {
     event match {
-      case s: TestSucceeded => {
+      case s: TestSucceeded =>
         val x = testPattern.findFirstMatchIn(s.testName).get
-        results(x.group(1)).append("yay")
-      }
-      case s: TestCanceled => {
+        if x.group(1) == "Jena" then println(x.group(1))
+        results(x.group(1)).append(formatResult(x.group(2), s.timeStamp, "passed"))
+      case s: TestCanceled =>
         val x = testPattern.findFirstMatchIn(s.testName).get
-        results(x.group(1)).append("nay")
-      }
-      case s: TestFailed => {
+        results(x.group(1)).append(formatResult(x.group(2), s.timeStamp, "inapplicable"))
+      case s: TestFailed =>
         val x = testPattern.findFirstMatchIn(s.testName).get
-        results(x.group(1)).append("naaay")
-      }
+        results(x.group(1)).append(formatResult(x.group(2), s.timeStamp, "failed"))
       case _: SuiteCompleted =>
         results.foreach((name, sb) => {
-          println(name)
-          println(sb.toString())
+          println(s"Writing report for $name")
+          Files.createDirectories(Paths.get("integration-tests/target/reports/"))
+          Files.writeString(
+            Paths.get(
+              s"integration-tests/target/reports/Jelly-JVM_${renameIntegrations(name)} conformance report.ttl",
+            ),
+            prefixes + metadata + sb.toString(),
+          )
         })
       case _ =>
     }
